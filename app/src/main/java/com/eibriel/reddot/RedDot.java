6532da83bb8537bd47b3a107d7a0446a8489f063
Eibriel <eibriel@yahoo.com.ar>
2015-05-08 15:01:12 -0300

    Moving files

--- app/src/main/java/com/eibriel/reddot/RedDot.java
+++ app/src/main/java/com/eibriel/reddot/RedDot.java
@@ -54,9 +54,9 @@ public class RedDot extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
