afa41f9fa40c6c38f8b9de22d42cf4e1e084e327
Eibriel <eibriel@yahoo.com.ar>
2015-05-12 20:12:27 -0300

    Code improvements, adding amplitude sine and color sine

--- app/src/main/java/com/eibriel/reddot/DrawDot.java
+++ app/src/main/java/com/eibriel/reddot/DrawDot.java
@@ -12,8 +12,8 @@ import java.util.Random;
 
 public class DrawDot extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    //Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    //Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
     float centerX;
     float centerY;
     //float radius;
@@ -21,7 +21,7 @@ public class DrawDot extends View {
     public DrawDot(Context context) {
         super(context);
         paint.setColor(Color.RED);
-        paint2.setColor(Color.rgb(150, 0, 0));
+        //paint2.setColor(Color.rgb(150, 0, 0));
         //paint3.setColor(Color.rgb(80, 0, 0));
     }
 
@@ -33,41 +33,70 @@ public class DrawDot extends View {
     }
 
 
-    private float timeToRadius(long milliTime) {
-        long milliseconds4 = (milliTime % 1000000) / 100;
-        double milli4_float = (milliseconds4 / 10000.0) * (6 * Math.PI) ;
+    private float timeToSin(long milliTime,
+                            long offset,
+                            float amplitude,
+                            double truncate,
+                            double threshold) {
+        long milliseconds = (milliTime-offset) % (long)truncate;
+        double milli_float = (milliseconds / truncate) * (2 * Math.PI) ;
 
-        long milliseconds3 = (milliTime % 100000) / 10;
-        double milli3_float = (milliseconds3 / 10000.0) * (6 * Math.PI) ;
+        float sin = (float)Math.sin( milli_float );
+        sin = (sin + 1) / 2;
 
-        long milliseconds2 = milliTime % 10000;
-        double milli2_float = (milliseconds2 / 10000.0) * (6 * Math.PI) ;
+        //Log.d("TimerExample", "Sin... " + sin);
 
-        long milliseconds = milliTime % 1000;
-        double milli_float = (milliseconds / 1000.0) * (6 * Math.PI) ;
-
-        double sine1 = Math.sin(milli_float);
-        //sine1 = 0;
-        double sine2 = Math.sin(milli2_float);
-        double sine3 = Math.sin(milli3_float);
-        double sine4 = Math.sin(milli4_float);
-
-        double sine4_peak = sine4 - 0.5;
-        if (sine4_peak < 0.0) {
-            sine4_peak = 0.0;
+        if (sin < threshold) {
+            sin = 0;
         }
 
-        float radius_ = 100 +
-                ((float) sine1 * (3 * ((float)sine4_peak*10))) +
-                ((float) sine2 * 5) +
-                ((float) sine3 * 10) +
-                ((float) sine4 * 30);
-        //float radius_ = 100 + ((float)sine3 * 50 );
+        return sin * amplitude;
+    }
 
-        //Log.d("TimerExample", "Going for... " + sine4_peak);
+
+    private float timeToRadius(long milliTime) {
+
+        //1000 = 1 second
+        //10000 = 10 seconds
+        //100000 = 100 seconds, 1.66 minutes
+        //1000000 = 1000 seconds, 16.6 minutes
+        //10000000 = 2.7 hours
+        //100000000 = 27 hours
+        //1000000000 = 11.25 days
+
+        // Amplitude sines
+        float amp_sine1 = timeToSin(milliTime, 864,   1, 10000.0, 0);
+        float amp_sine2 = timeToSin(milliTime, 44579, 1, 100000.0, 0);
+        float amp_sine3 = timeToSin(milliTime, 3245,  1, 1000000.0, 0);
+        float amp_sine4 = timeToSin(milliTime, 1158,  1, 1000000.0, 0);
+        //
+        float amp_sine5 = timeToSin(milliTime, 864879546,  1, 100000000.0, 0);
+        float amp_sine6 = timeToSin(milliTime, 1457954771, 1, 100000000.0, 0);
+        float amp_sine7 = timeToSin(milliTime, 324545654,  1, 1000000000.0, 0);
+        float amp_sine8 = timeToSin(milliTime, 1158123221,  1, 1000000000.0, 0);
+
+        // Radius sines
+        float sine1 = timeToSin(milliTime, 1894,    10* amp_sine1*amp_sine5, 100.0, 0);
+        float sine2 = timeToSin(milliTime, 55689,   30* amp_sine2*amp_sine6, 1000.0, 0);
+        float sine3 = timeToSin(milliTime, 8897,    30*amp_sine3*amp_sine7, 10000.0, 0);
+        float sine4 = timeToSin(milliTime, 1124568, 30*amp_sine4*amp_sine8, 100000.0, 0);
+
+        float base = 100;
+        float radius_ = base + ((sine1 + sine2 + sine3 + sine4)*(base/50));
+
+        //Log.d("TimerExample", "Ampsine1 " + amp_sine1);
         return radius_;
     }
 
+
+    private int timeToColor(long milliTime) {
+        // Color sine
+        float r_color_sine = 200 + timeToSin(milliTime, 87956421, 55, 1000000.0, 0);
+        float g_color_sine = timeToSin(milliTime, 87956421, 55, 1000000.0, 0);
+        float b_color_sine = timeToSin(milliTime, 56421, 55, 1000000.0, 0);
+
+        return Color.rgb((int)r_color_sine, (int)g_color_sine, (int)b_color_sine);
+    }
 
 
     @Override
@@ -80,6 +109,7 @@ public class DrawDot extends View {
         Date time = new Date();
         //long rest = 14310300 * 100000;
         float radius = timeToRadius(time.getTime());
+        paint.setColor(timeToColor(time.getTime()));
         //float radius2 = timeToRadius( time.getTime()-500);
         //float radius3 = timeToRadius( time.getTime()-1000);
 
